0b72815ce000c58db6d5519a199b02aba6855e0e
AleksandreG01 <137819228+AleksandreG01@users.noreply.github.com>
2023-08-02 20:05:15 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -111,7 +111,7 @@ $('.carousel_4').owlCarousel({
 
 
 const counters = document.querySelectorAll(".count");
-const speed = 3000;
+const speed = 900;
 
 counters.forEach((counter) => {
   const updateCount = () => {
